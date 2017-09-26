6ad67c9fb96998861eba5ebaf5becff618af40dc
epointal <elisabeth.pointal@free.fr>
2017-09-26 11:36:41 +0200

    Add css in template

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ import PdfViewer from './formater-pdf-viewer.vue';
 ljs.addAliases({
 	dep: [//'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', 
 	   // 'https://rawgit.com/aeris-data/aeris-commons-fonts/develop/font/aeriscommmonsfonts.css',
-	    'viewer.css',
+	   // 'viewer.css',
 	    'https://cdnjs.cloudflare.com/ajax/libs/document-register-element/1.4.1/document-register-element.js',
 	    'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min.js',
 	    'https://cdnjs.cloudflare.com/ajax/libs/moment-range/3.0.3/moment-range.min.js'
