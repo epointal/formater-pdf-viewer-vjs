1d78089d1a9a637fa1f428d8347a2640e44a1e1a
epointal <elisabeth.pointal@free.fr>
2017-09-29 14:36:40 +0200

    last test?

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map';
-  module.exports.output.publicPath= PACKAGE.url+ buildName +'/dist/';
+  module.exports.output.publicPath= PACKAGE.url+ buildName +'/master/dist/';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
